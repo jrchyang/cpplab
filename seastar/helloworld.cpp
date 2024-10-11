59d1bf8bca188a47c26afe50708e331564e52dfe
jrchyang <yujrchyang@gmail.com>
2024-10-11 09:24:00 +0000

    update seastar helloworld file

--- seastar/helloworld.cpp
+++ seastar/helloworld.cpp
@@ -1,11 +1,28 @@
 #include <seastar/core/app-template.hh>
 #include <seastar/core/reactor.hh>
+#include <seastar/core/sleep.hh>
+#include <seastar/util/log.hh>
 #include <iostream>
+#include <stdexcept>
+
+seastar::future<> f() {
+	std::cout << "Hello world\n";
+	std::cout << seastar::smp::count << "\n";
+	std::cout << "Sleeping... " << std::flush;
+	using namespace std::chrono_literals;
+	seastar::sleep(200ms).then([] { std::cout << "200ms " << std::flush; });
+	seastar::sleep(100ms).then([] { std::cout << "100ms " << std::flush; });
+	return seastar::sleep(1s).then([] { std::cout << "Done.\n"; });
+}
 
 int main(int argc, char** argv) {
 	seastar::app_template app;
-	app.run(argc, argv, [] {
-		std::cout << "Hello world\n";
-		return seastar::make_ready_future<>();
-	});
+	try {
+		app.run(argc, argv, f);
+	} catch(...) {
+		std::cerr << "Couldn't start application: "
+			<< std::current_exception() << "\n";
+		return 1;
+	}
+	return 0;
 }
